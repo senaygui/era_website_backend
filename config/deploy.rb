8938fcd18c9da6a31590f5efcde130f9a668c2be
senaygui <senaygeletaw@gmail.com>
2025-10-20 13:07:25 +0300

    update

--- config/deploy.rb
+++ config/deploy.rb
@@ -18,7 +18,7 @@ set :passenger_restart_with_touch, true
 # set :deploy_to, "/home/deploy/yic_production"
 set :stages, %w[production staging]
 # append :linked_files, "config/database.yml", "config/master.key"
-append :linked_dirs, "log", "tmp/pids", "tmp/cache", "tmp/sockets", "vendor/bundle", "public/system", "public/uploads"
+append :linked_dirs, "log", "tmp/pids", "tmp/cache", "tmp/sockets", "vendor/bundle", "public/system", "public/uploads", "storage"
 
 set :keep_releases, 10
 set :whenever_identifier, -> { "#{fetch(:application)}_#{fetch(:stage)}" }
